5c85cc01ba8a22f80faf538863e02d521dc5a5f1
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-02 21:07:00 +0900

    Chore : JavaScript-Drum-Kit JS 파일 Strict 모드 설정

--- 01-JavaScript-Drum-Kit/js/index.js
+++ 01-JavaScript-Drum-Kit/js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
